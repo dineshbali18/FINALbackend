9109d07be7b251e25bc8f4ed928b65d027ad636b
balidinesh18 <67181766+balidinesh08@users.noreply.github.com>
2022-03-24 07:35:56 +0530

    initial

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express=require('express')
 const mongoose=require('mongoose')
 const bodyParser=require('body-parser')
@@ -6,9 +7,20 @@ const cors=require('cors')
 const sectionRoutes=require('./routes/section')
 const teachersRoutes=require('./routes/teachers')
 
-mongoose.connect('mongodb+srv://Bali:DiNeSh5@cluster0.j1daf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',()=>{
-    console.log("DB Connected..........")
-})
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useCreateIndex: true
+  })
+  .then(() => {
+    console.log("DB CONNECTED");
+  })
+  .catch((e)=>{
+      console.log(e);
+      console.log("DB NOT CONNECTED SUCCESFULLY");
+  });
+
 
 const app=express();
 
@@ -20,6 +32,6 @@ app.use("/api",sectionRoutes);
 // app.use("/api",teachersRoutes);
 
 
-app.listen(3000,()=>{
+app.listen(process.env.PORT,()=>{
     console.log("App is running..........")
 })
